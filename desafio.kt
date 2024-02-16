a3aeb0a30570cf668fa1f905e7faa7eaf530ffca
Tiago Freitas <tiagofreitasce@gmail.com>
2024-02-16 15:37:12 -0300

    Exemplo Kotlin

--- desafio.kt
+++ desafio.kt
@@ -1,21 +1,48 @@
-// [Template no Kotlin Playground](https://pl.kotl.in/WcteahpyN)
+enum class Nivel { BASICO, INTERMEDIARIO, AVANCADO } 
 
-enum class Nivel { BASICO, INTERMEDIARIO, DIFICIL }
+data class Usuario(val nome: String, val email: String, val senha: String)
 
-class Usuario
+data class ConteudoEducacional(var nome: String, var duracao: Int = 1, val nivel: Nivel = Nivel.BASICO)
 
-data class ConteudoEducacional(var nome: String, val duracao: Int = 60)
-
-data class Formacao(val nome: String, var conteudos: List<ConteudoEducacional>) {
+data class Formacao(val nome: String, val nivel: Nivel, var duracao: Int = 52, var conteudos: List<ConteudoEducacional>) {
 
     val inscritos = mutableListOf<Usuario>()
     
     fun matricular(usuario: Usuario) {
-        TODO("Utilize o parâmetro $usuario para simular uma matrícula (usar a lista de $inscritos).")
+        inscritos.add(usuario)
     }
 }
 
-fun main() {
-    TODO("Analise as classes modeladas para este domínio de aplicação e pense em formas de evoluí-las.")
-    TODO("Simule alguns cenários de teste. Para isso, crie alguns objetos usando as classes em questão.")
+val conteudos = listOf(
+    ConteudoEducacional("Principios", 7),
+    ConteudoEducacional("Documentação", 13),
+    ConteudoEducacional("Logica", 6),
+    ConteudoEducacional("Padrões de Projeto", 5),
+    ConteudoEducacional("Banco de Dados", 6),
+    ConteudoEducacional("Spring Boot", 10)
+)
+
+fun main() { 
+    
+    val formacao = Formacao("Backend com Kotlin", Nivel.BASICO, 47, conteudos)
+    
+    formacao.matricular(Usuario("Tiago", "tiago@gmail.com", "123"))
+    formacao.matricular(Usuario("Caio", "caio@gmail.com", "456"))
+    formacao.matricular(Usuario("Maria", "maria@gmail.com", "987"))
+    
+    println("-----------------------------------------------------------------")
+    println("Formação: ${formacao.nome} ")
+    println("Duração ${formacao.duracao} horas")
+    println("Nivel: ${formacao.nivel.name}")
+    println("-----------------------------------------------------------------")
+    
+    formacao.conteudos.forEach{
+    	println("*Conteudo: ${it.nome} ${it.duracao} hrs ${it.nivel.name}")
+    }
+    
+    println("-----------------------------------------------------------------")
+    formacao.inscritos.forEach{
+    	println("Usuario inscrito: ${it.nome}")    
+    }
+  
 }
